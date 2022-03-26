17b6797dbfb4c5c964c8f093602db56a4203251b
Henry Oladeji <henryoladeji2007@gmail.com>
2022-03-26 20:56:52 +0000

    Added Frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,169 @@
 <template>
-  <Tutorial/>
+  <!-- site wrapper -->
+<div class="site-wrapper">
+
+	<div class="main-overlay"></div>
+
+  <Header />
+
+	<!-- section main content -->
+	<section class="main-content">
+		<div class="container-xl">
+
+			<div class="row gy-4">
+
+				<div class="col-lg-8">
+
+					<!-- section header -->
+					<div class="section-header">
+						<h3 class="section-title">Latest Posts</h3>
+						<img src="images/wave.svg" class="wave" alt="wave" />
+					</div>
+
+					<div class="padding-30 rounded bordered">
+
+						<div class="row">
+							
+							<div class="col-md-12 col-sm-6" v-for="(front, i) in frontpage.data" :key="i">
+								<!-- post -->
+								<div class="post post-list clearfix">
+									<div class="thumb rounded">
+										<span class="post-format-sm">
+											<i class="icon-picture"></i>
+										</span>
+										<a href="blog-single.html">
+											<div class="inner">
+												<img src="https://monstajamss.com/wp-content/uploads/2022/03/coi-leray-375x195.jpg" alt="post-title" />
+											</div>
+										</a>
+									</div>
+									<div class="details">
+										<ul class="meta list-inline mb-3">
+											<li class="list-inline-item text-uppercase"><a href="#"><img src="images/other/author-sm.png" class="author text-uppercase" alt="author"/>{{ front.username }}</a></li>
+											<li class="list-inline-item"><a href="#">Trending</a></li>
+											<li class="list-inline-item">{{ front.created_at }}</li>
+										</ul>
+										<h5 class="post-title"><NuxtLink :to="'' + front.slug">{{ front.title }}</NuxtLink></h5>
+										<p class="excerpt mb-0">{{ front.excerpt }}</p>
+										<div class="post-bottom clearfix d-flex align-items-center">
+											<div class="social-share me-auto">
+												<button class="toggle-button icon-share"></button>
+												<ul class="icons list-unstyled list-inline mb-0">
+													<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+													<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+													<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+													<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+													<li class="list-inline-item"><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+													<li class="list-inline-item"><a href="#"><i class="far fa-envelope"></i></a></li>
+												</ul>
+											</div>
+											<div class="more-button float-end">
+												<a href="blog-single.html"><span class="icon-options"></span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+						</div
+						>
+						<!-- load more button -->
+						<div class="text-center">
+							<button class="btn btn-simple">Load More</button>
+						</div>
+
+					</div>
+
+				</div>
+				<Sidebar />
+
+			</div>
+
+		</div>
+	</section>
+
+	<!-- instagram feed -->
+	<div class="instagram">
+		<div class="container-xl">
+			<!-- button -->
+			<a href="#" class="btn btn-default btn-instagram">@Katen on Instagram</a>
+			<!-- images -->
+			<div class="instagram-feed d-flex flex-wrap">
+				<div class="insta-item col-sm-2 col-6 col-md-2">
+					<a href="#">
+						<img src="images/insta/insta-1.jpg" alt="insta-title" />
+					</a>
+				</div>
+				<div class="insta-item col-sm-2 col-6 col-md-2">
+					<a href="#">
+						<img src="images/insta/insta-2.jpg" alt="insta-title" />
+					</a>
+				</div>
+				<div class="insta-item col-sm-2 col-6 col-md-2">
+					<a href="#">
+						<img src="images/insta/insta-3.jpg" alt="insta-title" />
+					</a>
+				</div>
+				<div class="insta-item col-sm-2 col-6 col-md-2">
+					<a href="#">
+						<img src="images/insta/insta-4.jpg" alt="insta-title" />
+					</a>
+				</div>
+				<div class="insta-item col-sm-2 col-6 col-md-2">
+					<a href="#">
+						<img src="images/insta/insta-5.jpg" alt="insta-title" />
+					</a>
+				</div>
+				<div class="insta-item col-sm-2 col-6 col-md-2">
+					<a href="#">
+						<img src="images/insta/insta-6.jpg" alt="insta-title" />
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <Footer />
+
+</div><!-- end site wrapper -->
+
 </template>
 
 <script>
+import Header from '@/components/Front/Header'
+import Sidebar from '@/components/Front/Sidebar'
+import Footer from '@/components/Front/Footer'
 export default {
-  name: 'IndexPage'
+  // middleware: 'auth',
+  name: 'MainSite',
+  components:{
+    Header,
+    Sidebar,
+    Footer,
+  },
+  data: () => ({
+    frontpage: []
+  }),
+  async fetch() {
+    const response = await this.$axios.get('/api/frontpage')
+    this.frontpage = response.data
+  },
+  head(){
+    return{
+      title: 'Blog'
+    }
+  },
+  methods: {
+    
+  }
 }
 </script>
+
+
+<style scoped>
+@import '~/assets/front/css/bootstrap.min.css';
+@import '~/assets/front/css/all.min.css';
+@import '~/assets/front/css/slick.css';
+@import '~/assets/front/css/simple-line-icons.css';
+@import '~/assets/front/css/style.css';
+</style>
--- pages/posts/create.vue
+++ pages/posts/create.vue
@@ -106,6 +106,54 @@
                         </div>
                       </div>
                     </div>
+                     <div class="flex flex-wrap">
+                      <div class="w-full lg:w-12/12 px-4">
+                        <div class="relative w-full mb-3">
+                          <label
+                            class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Fetured Image
+                          </label>
+                          <input 
+                            type="file"
+                            class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          >
+                        </div>
+                      </div>
+                    </div>
+                    <div class="flex flex-wrap">
+                      <div class="w-full lg:w-12/12 px-4">
+                        <div class="relative w-full mb-3">
+                          <label
+                            class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Main Post Image (1)
+                          </label>
+                          <input 
+                            type="file"
+                            class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          >
+                        </div>
+                      </div>
+                    </div>
+                    <div class="flex flex-wrap">
+                      <div class="w-full lg:w-12/12 px-4">
+                        <div class="relative w-full mb-3">
+                          <label
+                            class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Main Post Image (2)
+                          </label>
+                          <input 
+                            type="file"
+                            class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          >
+                        </div>
+                      </div>
+                    </div>
                     <div class="rounded-t mb-0 px-6 py-6">
                       <div class="text-center flex justify-between">
                         <NuxtLink class="text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150 font-bold bg-pink-500"
