5d6fdb8d250756e11cb126a52f8b0c0d7449616f
Henry Oladeji <henryoladeji2007@gmail.com>
2022-06-24 15:30:55 +0100

    Added API Link

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,7 +99,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost',
+    // baseURL: 'http://localhost',
+    baseURL: 'https://api.japachronicles.xyz',
     credentials: true
   },
 
