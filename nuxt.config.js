da9c646b95bbf7ebfc47954fd840357d65579b21
Henry Oladeji <henryoladeji2007@gmail.com>
2022-06-25 11:22:16 +0100

    Minor Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,7 @@ export default {
             url: '/sanctum/csrf-cookie'
           },
           login: {
-            url: '/api/login',
+            url: '/login',
           },
           register: {
             url: '/api/register',
@@ -76,7 +76,7 @@ export default {
             url: '/api/logout',
           },
           user: {
-            url: '/api/user',
+            url: '/user',
           }
         },
         user: {
